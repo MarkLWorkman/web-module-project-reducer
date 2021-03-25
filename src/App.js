ad70569328782e17f0a5d3d382373e3759eebe8d
unknown <calanhaga@aim.com>
2021-03-24 22:12:50 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -37,48 +37,60 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
               <span id="operation">
-                <b>Operation:</b> X
+                <b>Operation:</b> {state.operation}
               </span>
               <span id="memory">
-                <b>Memory:</b> 0
+                <b>Memory:</b> {state.memory}
               </span>
             </div>
 
             <div className="row">
-              <CalcButton value={"M+"} />
-              <CalcButton value={"MR"} />
-              <CalcButton value={"MC"} />
+              <CalcButton
+                value={"M+"}
+                onClick={() => dispatch(recordMemory())}
+              />
+              <CalcButton
+                value={"MR"}
+                onClick={() => dispatch(recallMemory())}
+              />
+              <CalcButton
+                value={"MC"}
+                onClick={() => dispatch(clearMemory())}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={() => eventHandler(1)} />
+              <CalcButton value={2} onClick={() => eventHandler(2)} />
+              <CalcButton value={3} onClick={() => eventHandler(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={() => eventHandler(4)} />
+              <CalcButton value={5} onClick={() => eventHandler(5)} />
+              <CalcButton value={6} onClick={() => eventHandler(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={() => eventHandler(7)} />
+              <CalcButton value={8} onClick={() => eventHandler(8)} />
+              <CalcButton value={9} onClick={() => eventHandler(9)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton value={"+"} onClick={() => operationHandler("+")} />
+              <CalcButton value={"*"} onClick={() => operationHandler("*")} />
+              <CalcButton value={"-"} onClick={() => operationHandler("-")} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                value={"CE"}
+                onClick={() => dispatch(clearDisplay())}
+              />
             </div>
           </form>
         </div>
